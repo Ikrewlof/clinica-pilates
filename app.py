f23dc3a574b10a5d1977b91ad76da56638fdf172
David Merino <d-mer@Portdml.ipd-sa.es>
2026-02-19 09:56:34 +0100

    modicacion rutas7

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ import os
 
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1, x_prefix=1)
 app.secret_key = "clave_secreta_clinica"
 
 
